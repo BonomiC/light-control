b01bd2cadbc5ef29f0e4c8809c744b17ec73b910
Christopher Bonomi <bonomi.c11@gmail.com>
2019-01-28 19:18:40 -0500

    Working on post request

--- server/index.js
+++ server/index.js
@@ -1,13 +1,20 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 const port = 5000;
 
 app.get('/', (req, res) => res.send('Hello world'));
 
 app.post('/test', (req, res) => {
-	console.log("Pressed");
-	res.send("here");
+	console.log('Pressed');
+	// console.log(req);
+	res.send(req.body);
 });
 
 app.listen(port, () => console.log(`Example app listening on  http://localhost:${port}`));
--- single_button_client/script.js
+++ single_button_client/script.js
@@ -1,65 +1,21 @@
 window.onload = function() {
-	var config = {
-		apiKey: "AIzaSyBNyQ5Kb4RgRrCjR5usr4RiwYgn7J94K0E",
-		authDomain: "light-control-c46ac.firebaseapp.com",
-		databaseURL: "https://light-control-c46ac.firebaseio.com",
-		projectId: "light-control-c46ac",
-		storageBucket: "light-control-c46ac.appspot.com",
-		messagingSenderId: "490129759068"
-	};
-	firebase.initializeApp(config);
-
-	const database = firebase.database();
-	const ref = database.ref('data')
-
-	// console.log(database);
-	// console.log(ref);
-
-	// var data = {
-	// 	name: 'ButtonPressed',
-	// 	value: false
-	// };
 
 	document.querySelector('form').addEventListener('submit', (event) => {
 		event.preventDefault();
 
 		let data = {name: "Chris"};
 
+		// var xhr = new XMLHttpRequest();
+		// xhr.open("POST", "http://163.118.116.1:8081/test", true);
+		// xhr.send(JSON.stringify(data));
+
+		// console.log('Press');
+
 		fetch("http://localhost:5000/test", {
 			method: "POST",
-			body: JSON.stringify(data)
+			body: JSON.stringify({name: 1})
 		}).then(res => {
-			console.log(res);
-		});
-
-		//ref.push(data);
-
-		// ref.on('child_changed', (snapshot) => {
-		// 	var data = snapshot.val();
-		// 	console.log(data.value);
-		// }, (err) => {
-		// 	console.error(err);
-		// });
-
-		ref.once('value').then(function(snapshot) {
-			var data = snapshot.val();
-			var keys = Object.keys(data);
-
-			for(var i = 0; i < keys.length; i++) {
-				if(data[keys[i]].name == 'ButtonPressed') {
-					database.ref('data/' + keys[0]).set({
-						name: 'ButtonPressed',
-						value: true
-					});
-
-					setTimeout(() => {
-						database.ref('data/' + keys[0]).set({
-							name: 'ButtonPressed',
-							value: false
-						});
-					}, 50);
-				}
-			}
+			console.log(res.body);
 		});
 	});
 };
