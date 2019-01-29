869072c6d5f0a36875f631f37bcfbd36cab643ea
Christopher Bonomi <bonomi.c11@gmail.com>
2019-01-29 12:44:26 -0500

    Began parsing data

--- local-server/index.js
+++ local-server/index.js
@@ -1,19 +1,25 @@
 var io = require('socket.io-client');
 var socket = io.connect("https://node-server-test.now.sh", {
-    reconnection: true
+	reconnection: true
 });
 
 // var socket = io.connect("http://localhost:5000", {
-    // reconnection: true
+// reconnection: true
 // });
+
+var lightData;
 
 socket.on('connect', () => {
 	console.log('Connected to server!');
 });
 
 socket.once('connect', function () {
-    socket.on('message', function (data) {
-        console.log('message from the server:', JSON.parse(data));
-        // socket.emit('serverEvent', "thanks server! for sending '" + data + "'");
-    });
+	socket.on('message', function (data) {
+		lightData = JSON.parse(data);
+		console.log('Message from the server:', lightData);
+
+		if(lightData.colors) {
+			console.log('Number of colors:', lightData.colors.length);
+		}
+	});
 });
